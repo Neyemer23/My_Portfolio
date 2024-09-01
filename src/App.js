ca344f43578921d6d84a2dfddf98eed42d11d264
Neyemer Silva - Freelancer <yemersilva.freelancer23@gmail.com>
2024-09-01 12:04:43 +0100

    Add up

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
